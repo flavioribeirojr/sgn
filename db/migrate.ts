38e472d38f9f25829a6756f8349517ab8925ede1
Flavio Ribeiro <flavioribeirojr@outlook.com>
2024-06-28 17:18:59 -0300

    add database migration npm script

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,4 +1,3 @@
-import { env } from 'config/env'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
@@ -6,7 +5,7 @@ import path from 'path'
 import * as schema from './schema'
 
 (async () => {
-  const migrationClient = postgres(env.DATABASE_CONNECTION_URL, { max: 1 })
+  const migrationClient = postgres(process.env.DATABASE_CONNECTION_URL as string, { max: 1 })
   const db = drizzle(migrationClient, { logger: true, schema })
   await migrate(db, { migrationsFolder: path.join(__dirname, 'drizzle') })
   await migrationClient.end()
