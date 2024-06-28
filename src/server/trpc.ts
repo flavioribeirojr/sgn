448b8b77b2d2e7461ea2190c57a6b9acc5097726
Flavio Ribeiro <flavioribeirojr@outlook.com>
2024-06-28 15:33:21 -0300

    add console.log for trpc error formater

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,13 @@
 import { initTRPC } from '@trpc/server'
 
-const t = initTRPC.create()
+const t = initTRPC.create({
+  errorFormatter: opts => {
+    const { shape, error } = opts
+    console.log(error.cause)
+
+    return shape
+  }
+})
 
 export const router = t.router
 export const publicProcedure = t.procedure
